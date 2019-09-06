ddcd658937c20145d9c7cdff3acea02efbee8ae9
ShaunGhosh <sounakghosh@umass.edu>
2019-09-06 14:23:54 -0400

    Seconds Converter

--- HW 1/SecondsConverter.py
+++ HW 1/SecondsConverter.py
@@ -1,21 +1,21 @@
-def SecondsConverter(seconds):
-    NumberOfDays = int(seconds / 86400)
-    remainingSeconds = seconds % 86400
-    NumberOfHours = int(remainingSeconds/3600)
-    remainingSeconds = remainingSeconds % 3600
-    NumberOfMinutes = int(remainingSeconds/60)
-    remainingSeconds = remainingSeconds % 60
+def SecondsConverter():
+    seconds = int(input('Enter Seconds: '))
+    numberofdays = int(seconds / 86400)
+    remainingseconds = seconds % 86400
+    numberofhours = int(remainingseconds/3600)
+    remainingseconds = remainingseconds % 3600
+    numberofminutes = int(remainingseconds/60)
+    remainingseconds = remainingseconds % 60
 
-    if NumberOfDays >=1:
-        print('{} Days {} Hours {} Minutes {} Seconds'.format(NumberOfDays, NumberOfHours, NumberOfMinutes, remainingSeconds))
-    elif NumberOfHours >= 1:
-        print('{} Hours {} Minutes {} Seconds'.format(NumberOfHours, NumberOfMinutes, remainingSeconds))
-    elif NumberOfMinutes >= 1:
-        print('{} Minutes {} Seconds'.format(NumberOfMinutes, remainingSeconds))
+    if numberofdays >= 1:
+        print('{} Days {} Hours {} Minutes {} Seconds'.format(numberofdays, numberofhours, numberofminutes, remainingseconds))
+    elif numberofhours >= 1:
+        print('{} Hours {} Minutes {} Seconds'.format(numberofhours, numberofminutes, remainingseconds))
+    elif numberofminutes >= 1:
+        print('{} Minutes {} Seconds'.format(numberofminutes, remainingseconds))
     else:
-        print('{} Seconds'.format(remainingSeconds))
+        print('{} Seconds'.format(remainingseconds))
 
 
+SecondsConverter()
 
-
-days = SecondsConverter(3500000)
