de5c93cd2defd23f414c7f7d622d81fc774952fe
ShaunGhosh <sounakghosh@umass.edu>
2019-09-09 22:13:35 -0400

    Tic-Tac-Toe Game

--- HW 1/TicTacToe-Game.py
+++ HW 1/TicTacToe-Game.py
@@ -1,14 +1,16 @@
 """tictactoe game for 2 players"""
 
+# Initializes the list for the positions of the player objects
 choices = []
 
-for x in range(1, 10):
-    choices.append(x)
+for x in range(1, 10):  # iterates through the range of 1 to 9
+    choices.append(x)   # appends an element to the end of the list
 
-playerOneTurn = True
-winner = False
+playerOneTurn = True    # sets playerOneTurn to true for the first player to have the first move
+winner = False          # sets winner to false
 
 
+# printboard method sets up the tic-tac-toe board for the user
 def printboard():
     print('\n -----')
     print('|', choices[0], '|', choices[1], '|', choices[2], '|')
@@ -19,31 +21,33 @@ def printboard():
     print(' -----\n')
 
 
+#  iterates through the list to see if any of the conditions winner is true or not
 while not winner:
     printboard()
 
-    if playerOneTurn:
+    if playerOneTurn:                             # prints the which player's turn is it?
         print("Player 1:")
     else:
         print("Player 2:")
 
     try:
-        choice = int(input(">> "))
+        choice = int(input(">> "))                # takes in the input from the players
     except:
         print("please enter a valid field")
         continue
 
-    if choices[choice - 1] == 'X' or choices[choice - 1] == 'O':
+    if choices[choice - 1] == 'X' or choices[choice - 1] == 'O':    # prevents illegal assignment of values
         print("illegal move, please try again")
         continue
 
     if playerOneTurn:
-        choices[choice - 1] = 'X'
+        choices[choice - 1] = 'X'           # Assigns the player one's choice to the corresponding index in the list
     else:
-        choices[choice - 1] = 'O'
+        choices[choice - 1] = 'O'           # Assigns the player two's choice to the corresponding index in the list
 
-    playerOneTurn = not playerOneTurn
+    playerOneTurn = not playerOneTurn       # changes the turn of the player
 
+    # conditions for checking if a player has won the game with a 3 in a row
     for x in range(0, 3):
         y = x * 3
         if choices[y] == choices[(y + 1)] and choices[y] == choices[(y + 2)]:
@@ -58,4 +62,7 @@ while not winner:
         winner = True
         printboard()
 
+# the output statement stating which player has won based on which player had the last move
 print("Player " + str(int(playerOneTurn + 1)) + " wins!\n")
+
+
